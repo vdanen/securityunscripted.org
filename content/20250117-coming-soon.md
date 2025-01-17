3c2a65abf9ee73b306ba5a67409718c669b465e3
Vincent Danen <vdanen@redhat.com>
2025-01-17 15:38:59 -0700

    initial commit

---
+++ content/20250117-coming-soon.md
@@ -0,0 +1,5 @@
+Title: Coming soon!
+Date: 2021-01-17
+Category: Announcement
+
+Soon, my friends, soon...
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#AUTHOR = 'Vincent Danen'
+AUTHOR = 'Mystery Person'
+SITENAME = 'Security Unscripted'
+SITEURL = "https://securityunscripted.org/"
+
+PATH = "content"
+
+TIMEZONE = 'America/Edmonton'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+## Blogroll
+#LINKS = (
+#    ("Pelican", "https://getpelican.com/"),
+#)
+#
+## Social widget
+#SOCIAL = (
+#    ("You can add links in your config file", "#"),
+#    ("Another social link", "#"),
+#)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
